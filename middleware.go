69de8b5ad38b18925bf4a20c5d90733405b7b0b8
Samuel Berthe <dev@samuel-berthe.fr>
2024-02-12 12:05:10 +0100

    fix: dedup "headers" attribute

--- middleware.go
+++ middleware.go
@@ -173,12 +173,16 @@ func NewWithConfig(logger *slog.Logger, config Config) fiber.Handler {
 
 		// request headers
 		if config.WithRequestHeader {
+			kv := []any{}
+
 			for k, v := range c.GetReqHeaders() {
 				if _, found := HiddenRequestHeaders[strings.ToLower(k)]; found {
 					continue
 				}
-				requestAttributes = append(requestAttributes, slog.Group("header", slog.Any(k, v)))
+				kv = append(kv, slog.Any(k, v))
 			}
+
+			requestAttributes = append(requestAttributes, slog.Group("header", kv...))
 		}
 
 		if config.WithUserAgent {
@@ -197,12 +201,16 @@ func NewWithConfig(logger *slog.Logger, config Config) fiber.Handler {
 
 		// response headers
 		if config.WithResponseHeader {
+			kv := []any{}
+
 			for k, v := range c.GetRespHeaders() {
 				if _, found := HiddenResponseHeaders[strings.ToLower(k)]; found {
 					continue
 				}
-				responseAttributes = append(responseAttributes, slog.Group("header", slog.Any(k, v)))
+				kv = append(kv, slog.Any(k, v))
 			}
+
+			responseAttributes = append(responseAttributes, slog.Group("header", kv...))
 		}
 
 		attributes := append(
